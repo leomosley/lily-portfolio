aaf0fb17cdb553e8dc0f55a98223fd2409347ee1
leomosley <leo.mosley2@gmail.com>
2023-12-14 16:52:39 +0000

    Updated config + gitingore

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
+  distDir: 'dist',
 }
+
+module.exports = nextConfig
